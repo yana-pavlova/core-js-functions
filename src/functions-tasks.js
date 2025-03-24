353d882c4122a95bc57a117e3512bb4f8fd0aee6
Yana Pavlova <yn-pvlv@yandex.ru>
2025-03-24 11:41:22 +0300

    feat: implement memoize function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -110,8 +110,13 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+
+  return function cacheFunc() {
+    if (!cache) cache = func();
+    return cache;
+  };
 }
 
 /**
