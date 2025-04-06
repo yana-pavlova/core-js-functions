5383844358db9d93e9a55751a4c674e7c763bfe3
Yana Pavlova <yn-pvlv@yandex.ru>
2025-04-06 12:28:41 +0300

    feat: implement partialUsingArguments function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -196,8 +196,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function (...args2) {
+    const allArgs = args1.concat(args2);
+    return fn(...allArgs);
+  };
 }
 
 /**
