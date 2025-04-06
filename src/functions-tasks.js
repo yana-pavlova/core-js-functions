fb931328807bd046cffe94a44c0025bc6f9e4cad
Yana Pavlova <yn-pvlv@yandex.ru>
2025-04-06 12:28:00 +0300

    feat: implement getIdGeneratorFunction function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -217,8 +217,18 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  let isCalled = false;
+  return function () {
+    if (isCalled) {
+      count += 1;
+      return count;
+    }
+
+    isCalled = true;
+    return count;
+  };
 }
 
 module.exports = {
