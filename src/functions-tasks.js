202448fb06e0a909bd769683c38b29d350b51a3f
Yana Pavlova <yn-pvlv@yandex.ru>
2025-04-06 12:19:42 +0300

    feat: implement logger function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -173,8 +173,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argStr = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${argStr}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argStr}) ends`);
+    return result;
+  };
 }
 
 /**
